3aa1553650f95e34d4249f37eaddf1e49e2db42c
Felix Alimin <felixalimin@gmail.com>
2024-02-15 22:53:31 -0500

    added jsonwebtoken for authentication

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://limoncelli:${PASSWORD}@cluster0.wtksviv.mongodb.net/mern?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wtksviv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
